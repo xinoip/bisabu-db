dd700d06dbdbd8e743d6148ba146fa7ec2d9c251
akusgulu <akusgulu309@gmail.com>
2019-11-20 19:38:45 +0300

    Tokenize function completed.

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -1,12 +1,43 @@
 #include "lexer.h"
+#include <sstream>
+#include <iostream>
 
-Lexer* Lexer::instancePtr = nullptr;
+Lexer *Lexer::instancePtr = nullptr;
 
-Lexer* Lexer::instance(){
-    if(!instancePtr){
+Lexer *Lexer::instance()
+{
+    if (!instancePtr)
+    {
         instancePtr = new Lexer;
     }
 
     return instancePtr;
+}
+
+std::vector<std::string> Lexer::processInput(const std::string &input, const char tokenChar) 
+{
+    std::stringstream parser(input);
+    std::string token;
+    std::vector<std::string> tokens;
+
+
+    while(getline(parser,token,tokenChar)){
+        tokens.push_back(token);
+    }
+
+    for(int i=0;i<tokens.size();++i) std::cout << tokens[i] << std::endl;
+
+    return tokens;
+
+
+
+}
+
+void Lexer::resetHistory(){
+    commandHistory.clear();
+}
+
+void Lexer::printHistory(){
+
 }
 
